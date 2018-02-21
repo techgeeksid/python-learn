06efb2fd87c3ca289b1d58341e541ba1cfdf9f53
techgeeksid <k.siddhartha.varma@gmail.com>
2018-02-21 23:50:56 +0530

    update

--- ex5/ex5.py
+++ ex5/ex5.py
@@ -5,7 +5,7 @@ my_weight = 180
 my_eyes ='blue'
 my_teeth = 'white'
 my_hair = 'brown'
-
+ 
 print(f"let's talk about {my_name}.")
 print(f"he's {my_height} inches tall.")
 print(f"he's {my_weight} pounds heavy.")
---
+++ ex6/ex6.py
@@ -0,0 +1,22 @@
+types_of_people = 10
+x = f"There are {types_of_people} types of people."
+
+binery = "binery"
+do_not = "don't"
+y = f"Those who known {binery} and those who {do_not}"
+
+print(x)
+print(y)
+
+print(f"i said: {x}")
+print(f"I also said: {y}' ")
+
+hilarious = False
+joke_evaluation = "isn't that joke so funny?! {}"
+
+print(joke_evaluation.format(hilarious))
+
+w = "this is the left said of..."
+e = "a string with a right said."
+
+print(w + e)

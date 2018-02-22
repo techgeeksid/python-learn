1dc0bdf8f9fbdd113d25198e4ce5070a65a182d7
techgeeksid <k.siddhartha.varma@gmail.com>
2018-02-22 17:56:37 +0530

    update

---
+++ ex12/ex12.py
@@ -0,0 +1,5 @@
+age = input("how old are you?")
+height = input("how tall are you? ")
+weight = input("how much do you weight? ")
+
+print(f"So, you're {age} old,{height} tall and {weight} heavy.")
---
+++ ex13/ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+#read the wyss section for how to run this
+script, first, second, third = argv
+
+print("the script is called:", script)
+print("Your first variable is:", first)
+print("your secound variable is:", second)
+print("your third variable is:", third)
